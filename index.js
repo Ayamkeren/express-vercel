f02ccaef7c891c00e5b86610a1f1ddb17acfeeb4
abyghails <abyghail10@gmail.com>
2024-05-05 21:35:02 +0700

    Express Project with Express Generator

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-const express = require("express");
-
-const app = express();
-
-app.get("/", (req, res) => {
-  return res.json({ message: "Succes pakai nodemon" });
-});
-
-app.get("/users", (req, res) => {
-  return res.send("Daftar users");
-});
-
-
-app.listen(3000, () => {
-	console.log("Procces running at port 3000")
-})
+require("./bin/www")
